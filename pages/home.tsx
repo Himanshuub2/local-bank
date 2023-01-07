c9bf4f6b681f9eebb9238711ee9f460fecc84816
Himanshu K <2121997him@gmail.com>
2023-01-07 22:16:05 +0530

    removed carousal

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,6 +1,6 @@
 import Main from "../components/Main"
 import React from "react";
-import Carousal from "../components/Carousal";
+
 import NewCar from "../components/NewCar";
 
 
@@ -8,7 +8,7 @@ const Home = ()=>{
     return (
         <div className = " w-screen h-screen bg-[#cde9f2]">
              <NewCar/>
-            {/* <Carousal/> */}
+          
            
             <Main/>
         </div>
